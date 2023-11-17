dc69fc2c46254b37a62d6d4241a2da8884ddc67b
Victor Campos <victorhugo1512@outlook.com>
2023-11-17 16:57:38 -0300

    t

--- app.py
+++ app.py
@@ -3,53 +3,56 @@ from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 import pandas as pd
 import io
-from celery import Celery
 
 app = Flask(__name__)
 CORS(app, resources={r"/upload": {"origins": "https://jetcalcship.web.app"}})
 
-# Configure Celery
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
-
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
-
 @app.route('/upload', methods=['POST'])
+
+
 def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-
-    file = request.files['file']
-
-    if file.filename == '' or not file.filename.endswith('.xlsx'):
-        return jsonify({'error': 'Invalid file'}), 400
-
-    task = process_file.delay(file.read())
-    return jsonify({'task_id': task.id}), 202
-
-@celery.task(bind=True)
-def process_file(self, file_data):
     try:
-        excel_data = pd.read_excel(io.BytesIO(file_data))
-        validator = Validator(dataframe=excel_data)
-        validator.style_dataframe()
-        excel_data = validator.get_dataframe()
+        # Check if the POST request has the file part
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
 
-        output = io.BytesIO()
-        writer = pd.ExcelWriter(output, engine='openpyxl')
-        excel_data.to_excel(writer, index=False)
-        writer.close()
-        output.seek(0)
+        file = request.files['file']
 
-        # You might need to save the file somewhere and return a link or ID to download it
-        return send_file(output, download_name='planilha_atualizada.xlsx', as_attachment=True)
+        # Check if the file has a valid name and extension
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+
+        # Ensure the file is an Excel file
+        if file and file.filename.endswith('.xlsx'):
+            # Read the Excel file into a DataFrame
+            excel_data = pd.read_excel(file)
+
+            # Validator to check the excel data 
+            validator = Validator(dataframe=excel_data)
+            validator.style_dataframe()
+
+            # Check data
+            excel_data = validator.get_dataframe()
+
+            # Save the updated DataFrame to a new Excel file
+            output = io.BytesIO()
+            writer = pd.ExcelWriter(output, engine='openpyxl')
+
+            excel_data.to_excel(writer, index=False)
+            writer.close()
+            output.seek(0)
+
+            # Return the updated Excel file to the user
+            return send_file(output, download_name='planilha_atualizada.xlsx', as_attachment=True)
+
+        else:
+            return jsonify({'error': 'Invalid file format, must be .xlsx'}), 400
+
     except Exception as e:
-        return {'error': str(e)}
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
 
-            
