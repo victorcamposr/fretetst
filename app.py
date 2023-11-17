257f4ae795690032fd66ad723463a88b82c90f72
Victor Campos <victorhugo1512@outlook.com>
2023-11-17 14:43:07 -0300

    a

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import asyncio
 from models import Validator
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
@@ -8,11 +7,10 @@ import io
 app = Flask(__name__)
 CORS(app, resources={r"/upload": {"origins": "https://jetcalcship.web.app"}})
 
-def send_file_sync(output, download_name, as_attachment):
-    return send_file(output, download_name=download_name, as_attachment=as_attachment)
-
 @app.route('/upload', methods=['POST'])
-async def upload_file():
+
+
+def upload_file():
     try:
         # Check if the POST request has the file part
         if 'file' not in request.files:
@@ -29,7 +27,7 @@ async def upload_file():
             # Read the Excel file into a DataFrame
             excel_data = pd.read_excel(file)
 
-            # Validator to check the excel data
+            # Validator to check the excel data 
             validator = Validator(dataframe=excel_data)
             validator.style_dataframe()
 
@@ -44,23 +42,14 @@ async def upload_file():
             writer.close()
             output.seek(0)
 
-            # Save the file asynchronously using run_in_executor
-            loop = asyncio.get_event_loop()
-            response = await loop.run_in_executor(None, send_file_sync, output, 'planilha_atualizada.xlsx', True)
-
-            # Adicione os cabeçalhos CORS à resposta
-            response.headers.add("Access-Control-Allow-Origin", "https://jetcalcship.web.app")
-            response.headers.add("Access-Control-Allow-Methods", "POST")
-            response.headers.add("Access-Control-Allow-Headers", "Content-Type")
-
-            return response
+            # Return the updated Excel file to the user
+            return send_file(output, download_name='planilha_atualizada.xlsx', as_attachment=True)
 
         else:
             return jsonify({'error': 'Invalid file format, must be .xlsx'}), 400
 
     except Exception as e:
-        print(f"Erro: {str(e)}")  # Imprime o erro no console
-        return jsonify({'error': 'Internal Server Error'}), 500
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
